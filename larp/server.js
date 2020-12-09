705921bb5eaa30cb5970ca0c9b4540a90a37f137
tina458615 <69339812+tina458615@users.noreply.github.com>
2020-12-10 00:50:35 +0800

    delete

--- larp/server.js
+++
@@ -1,8 +0,0 @@
-var nodeStatic = require('node-static');
-var http = require('http');
-var file = new(nodeStatic.Server)();
-var app = http.createServer(function (req, res) {
-      file.serve(req, res);
-}).listen(8001);
-var PeerServer = require('peer').PeerServer;
-var server = PeerServer({port: 8002, path: '/myapp'});
